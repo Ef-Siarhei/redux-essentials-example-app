dfc8ce131e0fafde49b77ada87c956b08de99a1d
Siarhei Yafimenka <h1_n1@mail.ru>
2024-09-17 21:25:22 +0300

    export type from store .ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -23,4 +23,11 @@ export const store = configureStore({
     }
 })
 
+// Определить тип «store»
+export type AppStore = typeof store
 
+// Определите тип AppDispatch из самого store.
+export type AppDispatch = typeof store.dispatch
+
+// То же самое для типа RootState.
+export type RootState = ReturnType<typeof store.getState>
