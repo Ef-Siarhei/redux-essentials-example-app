c3a0b1b7753215334c5bd85241252d504da0790f
Siarhei Yafimenka <h1_n1@mail.ru>
2024-09-18 21:31:34 +0300

    add New Post

---
+++ src/features/posts/AddPostForm.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {useAppDispatch} from "@/app/hooks";
+import {type Post, postAdded} from "@/features/posts/postsSlise";
+import {nanoid} from "@reduxjs/toolkit";
+
+// Типы TS для ввода полей
+// См.: https://epicreact.dev/how-to-type-a-react-form-on-submit-handler/
+interface AddPostFormFields extends HTMLFormControlsCollection {
+  postTitle: HTMLInputElement
+  postContent: HTMLTextAreaElement
+}
+
+interface AddPostFormElements extends HTMLFormElement {
+  readonly elements: AddPostFormFields
+}
+
+const AddPostForm = () => {
+  // Получаем метод `dispatch` из хранилища
+  const dispatch = useAppDispatch()
+
+  const handleSubmit = (e: React.FormEvent<AddPostFormElements>) => {
+    // Запретить отправку на сервер
+    e.preventDefault()
+
+    const {elements} = e.currentTarget
+    const title = elements.postTitle.value
+    const content = elements.postContent.value
+
+    // Создаём объект post и отправляем действие `postAdded`
+    const newPost: Post = {
+      id: nanoid(),
+      title,
+      content
+    }
+
+    dispatch(postAdded(newPost))
+
+    e.currentTarget.reset()
+  }
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor={'postTitle'}>Post Title:</label>
+        <input type={'text'} id={'postTitle'} defaultValue={''} required/>
+        <label htmlFor={'postContent'}>Content:</label>
+        <textarea
+          id={'postContent'}
+          name={'postContent'}
+          defaultValue={''}
+          required
+        />
+        <button>Save Post</button>
+      </form>
+    </section>
+  )
+}
+
+export default AddPostForm
--- src/features/posts/postsSlise.ts
+++ src/features/posts/postsSlise.ts
@@ -1,24 +1,35 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 // Определить тип TS для данных, которые мы будем использовать.
 export interface Post {
-    id: string
-    title: string
-    content: string
+  id: string
+  title: string
+  content: string
 }
 
 // Создаем начальное значение состояния для редуктора с этим типом
 const initialState: Post[] = [
-    {id: '1', title: 'First Post!', content: 'Hello!'},
-    {id: '2', title: 'Second Post!', content: 'More text!'},
+  {id: '1', title: 'First Post!', content: 'Hello!'},
+  {id: '2', title: 'Second Post!', content: 'More text!'},
 ]
 
 // Создаем Slice и передаем исходное состояние
 const postsSlice = createSlice({
-    name: 'posts',
-    initialState,
-    reducers: {}
+  name: 'posts',
+  initialState,
+  reducers: {
+    // Объявляем «редуктор регистра» с именем postAdded.
+    // Тип `action.payload` будет объектом `Post`.
+    postAdded(state, action: PayloadAction<Post>) {
+      // «Мутировать» существующий массив эффектов, который
+      // здесь безопасен, потому что `createSlice` использует внутри Immer.
+      state.push(action.payload)
+    }
+  }
 })
+
+// Экспортируем автоматически созданный создатель действия с тем же именем
+export const {postAdded} = postsSlice.actions
 
 // Экспортируем сгенерированную функцию редуктора
 export default postsSlice.reducer
