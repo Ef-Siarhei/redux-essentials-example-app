7b56af6894340b2198812ff9b5204a673be735cc
Siarhei Yafimenka <h1_n1@mail.ru>
2024-09-18 19:59:47 +0300

    add comments to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import {Provider} from "react-redux";
 
 import App from './App'
 import {store} from "@/app/store";
 
-import { worker } from './api/server'
+import {worker} from './api/server'
 
 import './primitiveui.css'
 import './index.css'
 
-// Wrap app rendering, so we can wait for the mock API to initialize
+// Обернуть рендеринг приложения, чтобы мы могли дождаться инициализации макетного API.
 async function start() {
-  // Start our mock API server
-  await worker.start({ onUnhandledRequest: 'bypass' })
+  // Запускаем наш фиктивный сервер API
+  await worker.start({onUnhandledRequest: 'bypass'})
 
   const root = createRoot(document.getElementById('root')!)
 
   root.render(
     <React.StrictMode>
       <Provider store={store}>
-      <App />
+        <App/>
       </Provider>
     </React.StrictMode>,
   )
