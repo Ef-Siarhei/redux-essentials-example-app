ff292229fab1aa4ee613c2f3f5dc76e177d903fa
Siarhei Yafimenka <h1_n1@mail.ru>
2024-09-18 22:11:49 +0300

    add SinglePostPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import {Navbar} from './components/Navbar'
 import PostsList from "@/features/posts/PostsList";
 import AddPostForm from "@/features/posts/AddPostForm";
+import {SinglePostPage} from "@/features/posts/SinglePostPage";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
               </section>
             }
           ></Route>
+          <Route path={'/posts/:postId'} element={<SinglePostPage/>}></Route>
         </Routes>
       </div>
     </Router>
