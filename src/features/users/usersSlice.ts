1e988986970cd6fe26109ea271c7b50922041ba1
Siarhei Yafimenka <h1_n1@mail.ru>
2024-09-23 21:19:52 +0300

    add usersSlice.ts and connected to store.ts

---
+++ src/features/users/usersSlice.ts
@@ -0,0 +1,28 @@
+import {createSlice} from "@reduxjs/toolkit";
+import {RootState} from "@/app/store";
+
+interface User {
+  id: string
+  name: string
+}
+
+const initialState: User[] = [
+  {id: '0', name: 'Fredi Mercury'},
+  {id: '1', name: 'Merlin Munro'},
+  {id: '2', name: 'Ken Roy'}
+]
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+
+  }
+})
+
+export default usersSlice.reducer
+
+export const selectAllUsers = (state: RootState) => state.users
+
+export const selectUserById = (state: RootState, userId: string) =>
+  state.users.find(user => user.id === userId)
