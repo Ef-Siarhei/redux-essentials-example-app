e99e5eaf1c59884d84f9821352dc3abd788601c8
Siarhei Yafimenka <h1_n1@mail.ru>
2024-09-26 21:40:20 +0300

    add TimeAgo to SinglePostPage.tsx

--- src/features/posts/SinglePostPage.tsx
+++ src/features/posts/SinglePostPage.tsx
@@ -2,6 +2,7 @@ import {Link, useParams} from "react-router-dom";
 import {useAppSelector} from "@/app/hooks";
 import {selectPostById} from "@/features/posts/postsSlice";
 import {PostAuthor} from "@/features/posts/PostAuthor";
+import {TimeAgo} from "@/components/TimeAgo";
 
 export const SinglePostPage = () => {
   const {postId} = useParams()
@@ -22,6 +23,7 @@ export const SinglePostPage = () => {
         <h2>{post.title}</h2>
         <p className={'post-content'}>{post.content}</p>
         <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date}/>
         <br/>
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
