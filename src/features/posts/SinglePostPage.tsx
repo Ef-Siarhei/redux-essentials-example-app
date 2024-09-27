896adb7c862e0149e8afe3a4f8fe9215c19cba18
Siarhei Yafimenka <h1_n1@mail.ru>
2024-09-28 00:23:09 +0300

    add hide button 'Edit post' if post is not author

--- src/features/posts/SinglePostPage.tsx
+++ src/features/posts/SinglePostPage.tsx
@@ -4,11 +4,13 @@ import {selectPostById} from "@/features/posts/postsSlice";
 import {PostAuthor} from "@/features/posts/PostAuthor";
 import {TimeAgo} from "@/components/TimeAgo";
 import {ReactionButtons} from "@/features/posts/ReactionButtons";
+import {selectCurrentUsername} from "@/features/auth/authSlice";
 
 export const SinglePostPage = () => {
   const {postId} = useParams()
 
   const post = useAppSelector(state => selectPostById(state, postId!))
+  const currentUsername = useAppSelector(selectCurrentUsername)
 
   if (!post) {
     return (
@@ -18,18 +20,22 @@ export const SinglePostPage = () => {
     )
   }
 
+  const canEdit = currentUsername === post.user
+
   return (
     <section>
       <article className={'post'}>
         <h2>{post.title}</h2>
         <p className={'post-content'}>{post.content}</p>
-        <PostAuthor userId={post.user} />
+        <PostAuthor userId={post.user}/>
         <TimeAgo timestamp={post.date}/>
         <ReactionButtons post={post}/>
         <br/>
-        <Link to={`/editPost/${post.id}`} className="button">
-          Edit Post
-        </Link>
+        {canEdit &&
+          <Link to={`/editPost/${post.id}`} className="button">
+            Edit Post
+          </Link>
+        }
       </article>
     </section>
   )
