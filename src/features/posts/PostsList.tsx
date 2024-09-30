db1bd44d999a081e82e40f5d80b3e4ccd02271dd
Siarhei Yafimenka <h1_n1@mail.ru>
2024-09-30 21:03:16 +0300

    extract PostExcerpt from PostList

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -1,10 +1,28 @@
 import {useAppDispatch, useAppSelector} from "@/app/hooks";
 import {Link} from "react-router-dom";
-import {fetchPosts, selectAllPosts, selectPostsStatus} from "@/features/posts/postsSlice";
+import {fetchPosts, Post, selectAllPosts, selectPostsStatus} from "@/features/posts/postsSlice";
 import {PostAuthor} from "@/features/posts/PostAuthor";
 import {TimeAgo} from "@/components/TimeAgo";
 import {ReactionButtons} from "@/features/posts/ReactionButtons";
 import {useEffect} from "react";
+
+interface PostExcerptProps {
+  post: Post
+}
+
+  function PostExcerpt({post}: PostExcerptProps) {
+    return (
+      <article className={'post-excerpt'}>
+        <h3>
+          <Link to={`/posts/${post.id}`}>{post.title}</Link>
+        </h3>
+        <p className={'post-content'}>{post.content.substring(0, 100)}</p>
+        <PostAuthor userId={post.user}/>
+        <TimeAgo timestamp={post.date}/>
+        <ReactionButtons post={post}/>
+      </article>
+    )
+  }
 
 const PostsList = () => {
   const dispatch = useAppDispatch()
@@ -20,16 +38,9 @@ const PostsList = () => {
   // Сортирую посты относительно временной метки date
   const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
+
   const renderedPosts = orderedPosts.map(post => (
-    <article className={'post-excerpt'} key={post.id}>
-      <h3>
-        <Link to={`/posts/${post.id}`}>{post.title}</Link>
-      </h3>
-      <p className={'post-content'}>{post.content.substring(0, 100)}</p>
-      <PostAuthor userId={post.user}/>
-      <TimeAgo timestamp={post.date}/>
-      <ReactionButtons post={post}/>
-    </article>
+    <PostExcerpt post={post} key={post.id}/>
   ))
 
   return (
