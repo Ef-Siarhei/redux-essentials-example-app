660e1ef029f597dbe6015f25e978983a2a0c2c9c
Siarhei Yafimenka <h1_n1@mail.ru>
2024-10-03 23:13:47 +0300

    add marking notifications as read

--- src/features/notification/NotificationsList.tsx
+++ src/features/notification/NotificationsList.tsx
@@ -1,14 +1,25 @@
-import {useAppSelector} from "@/app/hooks";
-import {selectAllNotifications} from "@/features/notification/notificationsSlice";
+import {useAppDispatch, useAppSelector} from "@/app/hooks";
+import {allNotificationsRead, selectAllNotifications} from "@/features/notification/notificationsSlice";
 import {PostAuthor} from "@/features/posts/PostAuthor";
 import {TimeAgo} from "@/components/TimeAgo";
+import {useLayoutEffect} from "react";
+import classNames from "classnames";
 
 export const NotificationsList = () => {
+  const dispatch = useAppDispatch()
   const notification = useAppSelector(selectAllNotifications)
 
+  useLayoutEffect(() => {
+    dispatch(allNotificationsRead())
+  })
+
   const renderNotifications = notification.map(notification => {
+    const notificationClassname = classNames('notification', {
+      new: notification.isNew
+    })
+
     return (
-      <div key={notification.id} className={'notification'}>
+      <div key={notification.id} className={notificationClassname}>
         <div>
           <b>
             <PostAuthor userId={notification.user} showPrefix={false}/>
